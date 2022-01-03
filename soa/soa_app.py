fd02110a322bc2cc54721de5a82d2098c6936415
Mathias Frey <mathias.c.frey@gmail.com>
2022-01-03 21:39:52 +0100

    faust successfully listening to debezium's CDC

--- soa/soa_app.py
+++ soa/soa_app.py
@@ -1,25 +1,31 @@
 import faust
+import asyncio
 
 
-class Test(faust.Record):
-    msg: str
+class CurrentAccountCDC(faust.Record):
+    payload: dict
 
 
 app = faust.App('soa_app', broker='kafka://kafka:9092')
-topic = app.topic('test', value_type=Test)
+topic = app.topic('current_currentaccount', value_type=CurrentAccountCDC)
 
 
 @app.agent(topic)
 async def hello(messages):
     async for message in messages:
-        print(f'Received {message.msg}')
+        
+        # fake a long-running computation
+        # delegated to another machine (I/O idling)
+        await asyncio.sleep(5)
+
+        print(message.payload)
 
 
-@app.timer(interval=5.0)
-async def example_sender():
-    await hello.send(
-        value=Test(msg='Hello World!'),
-    )
+# @app.timer(interval=5.0)
+# async def example_sender():
+#     await hello.send(
+#         value=Test(msg='Hello World!'),
+#     )
 
 
 if __name__ == '__main__':
