7818705cf524ee3614b47dc35c2bc55302bb6977
Mathias Frey <mathias.c.frey@gmail.com>
2022-01-03 22:55:12 +0100

    Added - some (fake) calculations - an in-memory table - output to another stream

--- soa/soa_app.py
+++ soa/soa_app.py
@@ -1,25 +1,57 @@
 import faust
 import asyncio
+import random
 
+class AccountProcessed(faust.Record):
+    account: str
+    balance: int
+    modeloutput: float 
+
+# a bit of a weird deserialization hapening here...
+class CurrentAccountAfter(faust.Record):
+    id: int
+    account: str
+    balance: int
+
+class CurrentAccountPayload(faust.Record):
+    after: CurrentAccountAfter
 
 class CurrentAccountCDC(faust.Record):
-    payload: dict
+    payload: CurrentAccountPayload
 
 
-app = faust.App('soa_app', broker='kafka://kafka:9092')
+app = faust.App('soa_app', broker='kafka://kafka:9092', topic_partitions=1)
 topic = app.topic('current_currentaccount', value_type=CurrentAccountCDC)
+destination_topic = app.topic('account_processed', value_type=CurrentAccountCDC)
 
 
-@app.agent(topic)
+balance_stats = app.Table('balance_stats', default=int)
+
+@app.agent(topic, sink=[destination_topic])
 async def hello(messages):
     async for message in messages:
         
         # fake a long-running computation
         # delegated to another machine (I/O idling)
-        await asyncio.sleep(5)
+        await asyncio.sleep(3)
+        
 
-        print(message.payload)
+        try:
+            
+            account = message.payload.after.account
+            balance = message.payload.after.balance
 
+            # this is just an example that we can
+            # store in-memory
+            balance_stats[account] =  balance
+            print('Saved to evolving fact table')
+            print(balance_stats[account])
+
+        except:
+            # this was a delete event, there's no after
+            print('Encountered a delete')
+            raise
+        yield AccountProcessed(account=account, balance=balance, modeloutput=random.random())
 
 # @app.timer(interval=5.0)
 # async def example_sender():
