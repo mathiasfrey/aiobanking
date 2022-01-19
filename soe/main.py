f4679547061e6867a7963bc462934ed995c3b3da
Mathias Frey <mathias.c.frey@gmail.com>
2022-01-19 20:35:23 +0100

    added raw stream "/ws-raw"

--- soe/main.py
+++ soe/main.py
@@ -17,11 +17,10 @@ from pydantic import StrictStr
 app = FastAPI(title='aiobanking frontend')
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
-class ConsumerResponse(BaseModel):
+class ConsumerResponseProcessed(BaseModel):
     account: StrictStr
     balance: int
     modeloutput: float
-
 
 async def consume(consumer, topicname):
     async for msg in consumer:
@@ -74,11 +73,60 @@ class WebsocketConsumer(WebSocketEndpoint):
         self.counter = 0
         while True:
             data = await consume(self.consumer, topicname)
-            response = ConsumerResponse(topic=topicname, **json.loads(data))
+            response = ConsumerResponseProcessed(topic=topicname, **json.loads(data))
             logger.info(response)
             await websocket.send_text(f"{response.json()}")
             self.counter = self.counter + 1
 
+@app.websocket_route("/ws-raw")
+class WebsocketConsumer(WebSocketEndpoint):
+    """
+    R/T stream from CDC
+    """
+
+    async def on_connect(self, websocket: WebSocket) -> None:
+        topicname = 'current_currentaccount'
+
+        await websocket.accept()
+        await websocket.send_json({"Message: ": "connected"})
+
+        loop = asyncio.get_event_loop()
+        self.consumer = AIOKafkaConsumer(
+            topicname,
+            loop=loop,
+            client_id='aiobanking-soa',
+            bootstrap_servers="kafka:9092",
+            enable_auto_commit=False,
+        )
+
+        await self.consumer.start()
+
+        self.consumer_task = asyncio.create_task(
+            self.send_consumer_message(websocket=websocket, topicname=topicname)
+        )
+
+        logger.info("connected")
+
+    async def on_disconnect(self, websocket: WebSocket, close_code: int) -> None:
+        self.consumer_task.cancel()
+        await self.consumer.stop()
+        logger.info(f"counter: {self.counter}")
+        logger.info("disconnected")
+        logger.info("consumer stopped")
+
+    async def on_receive(self, websocket: WebSocket, data: typing.Any) -> None:
+        await websocket.send_json({"Message: ": data})
+
+    async def send_consumer_message(self, websocket: WebSocket, topicname: str) -> None:
+        self.counter = 0
+        while True:
+            data = await consume(self.consumer, topicname)
+            #response = ConsumerResponseRaw(topic=topicname, **json.loads(data))
+            #logger.info(response)
+            #await websocket.send_text(f"{response.json()}")
+            await websocket.send_text(data)
+            self.counter = self.counter + 1
+            
 @app.get("/table")
 def table():
 
